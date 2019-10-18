3176a006bf54d94700b263c73b98e6d9092605fe
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2019-10-18 10:17:28 -0400

    Change settings

--- django/config/settings/development.py
+++ django/config/settings/development.py
@@ -1,1 +1,16 @@
 from config.settings.common import *
+
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sub-tracker',
+        'USER': 'sub-tracker',
+        'PASSWORD': 'development',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
